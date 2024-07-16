19f3662bf4e5df63a20d0a002168412dcce40a60
Parthiv Mothipati <pmothipati@gmail.com>
2024-07-16 12:11:06 -0700

    classes

--- practice.dart
+++ practice.dart
@@ -1,11 +1,27 @@
-int fibonacci(int length) {
-  if (length == 0 || length == 1) return length;
-  return fibonacci(length - 1) + fibonacci(length - 2);
+class Person {
+  String name;
+  DateTime? birthday;
+  int? get birthYear => birthday?.year;
+
+  Person(this.name, this.birthday) {
+    // Initialization code
+  }
+  Person.born(String name) : this(name, null);
+
+  void describe() {
+    print('Person: $name');
+    // Workaround for type promotion
+    var birthday = this.birthday;
+    if (birthday != null) {
+      int years = DateTime.now().difference(birthday).inDays ~/ 365;
+      print('Born $birthYear ($years years ago)');
+    } else {
+      print('Not born yet');
+    }
+  }
 }
 
 void main() {
-  var numbers = List.generate(50, (i) => i);
-  for (final number in numbers) {
-    print(number.toString() + ': ' + fibonacci(number).toString());
-  }
+  var parthiv = Person('Parthiv', DateTime(2009, 2, 14));
+  parthiv.describe();
 }
