4b10feab8acb3db8c79a13722495ad6fda1b930c
Parthiv Mothipati <pmothipati@gmail.com>
2024-07-17 10:31:27 -0700

    throw exceptions

--- practice.dart
+++ practice.dart
@@ -1,26 +1,11 @@
-import 'dart:async';
-import 'dart:io';
-
-Future<void> createDescriptions(Iterable<String> fileNames) async {
-  for (final fileName in fileNames) {
-    try {
-      var file = File('$fileName.txt');
-      if (await file.exists()) {
-        var modified = await file.lastModified();
-        print(
-            'File for $fileName already exists. It was last modified on $modified');
-        continue;
-      }
-
-      await file.create();
-      await file.writeAsString('Start describing $fileName in this file.');
-      print('File $fileName created');
-    } on IOException catch (e) {
-      print('IOException: $e');
-    }
+void checkAstronauts(astronauts) {
+  if (astronauts == 0) {
+    throw StateError('No astronauts');
+  } else {
+    print('There are $astronauts astronauts!');
   }
 }
 
 void main() {
-  createDescriptions(['name', 'date']);
+  checkAstronauts(1);
 }
