f6eda4975a383c276fbd5695e20f65bebd327988
Parthiv Mothipati <pmothipati@gmail.com>
2024-07-17 10:28:31 -0700

    files and async

--- practice.dart
+++ practice.dart
@@ -1,10 +1,26 @@
-const second = Duration(seconds: 1);
+import 'dart:async';
+import 'dart:io';
 
-void printWithDelay(String message) async {
-  await Future.delayed(second);
-  print(message);
+Future<void> createDescriptions(Iterable<String> fileNames) async {
+  for (final fileName in fileNames) {
+    try {
+      var file = File('$fileName.txt');
+      if (await file.exists()) {
+        var modified = await file.lastModified();
+        print(
+            'File for $fileName already exists. It was last modified on $modified');
+        continue;
+      }
+
+      await file.create();
+      await file.writeAsString('Start describing $fileName in this file.');
+      print('File $fileName created');
+    } on IOException catch (e) {
+      print('IOException: $e');
+    }
+  }
 }
 
 void main() {
-  printWithDelay('message');
+  createDescriptions(['name', 'date']);
 }
